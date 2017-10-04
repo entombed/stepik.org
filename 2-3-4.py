6c2466d6e423f34003118ce1e74e7f81e3d4d847
Andrew Bondarenko <ent101@outlook.com>
2017-10-04 15:32:46 +0300

    done

---
+++ 2-3-4.py
@@ -0,0 +1,33 @@
+class multifilter:
+    def judge_half(pos, neg):
+        # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
+        return pos >= neg
+
+    def judge_any(pos, neg):
+        # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+        return pos >= 1
+
+    def judge_all(pos, neg):
+        # допускает элемент, если его допускают все функции (neg == 0)
+        return neg == 0 
+
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        # iterable - исходная последовательность
+        # funcs - допускающие функции
+        # judge - решающая функция
+        self.iterable = iterable
+        self.funcs = funcs
+        self.judge = judge
+
+    def __iter__(self):
+        # возвращает итератор по результирующей последовательности
+        for i in self.iterable:
+            pos = 0
+            neg = 0
+            for f in self.funcs:
+                if f(i):
+                    pos += 1
+                else:
+                    neg += 1
+            if self.judge(pos, neg):
+                yield i
