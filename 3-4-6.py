e11aca7fb677e93170450c50a25da7e963127419
Andrey Bondarenko <ent101@outlook.com>
2017-10-09 21:47:15 +0300

    done

--- 3-4-6.py
+++ 3-4-6.py
@@ -1,23 +1,19 @@
 import requests
 import re
 urlS = 'https://stepic.org/media/attachments/lesson/24472/sample0.html'
-urlF = 'https://stepic.org/media/attachments/lesson/24472/sample1.html'
+urlF = 'https://stepic.org/media/attachments/lesson/24472/sample2.html'
 
-def getUrl(url):
+
+reStr = r'href=[\'"]?([^\'" >]+)'
+req = requests.get(urlS)
+reqText = req.text
+reRez = re.findall(reStr, reqText)
+rez = 'No'
+for url in reRez:
+#    print(url)
     req = requests.get(url)
-    reqText  = req.text
-    reStr = r'https://stepic.org/.*\d.html'
+    reqText = req.text
     reRez = re.findall(reStr, reqText)
-    return reRez[0]
-count = 0
-while True:
-    urlS = getUrl(urlS)
-    if (urlS == urlF and count > 3) or count > 3:
-        rezText = 'No'
-        break
-    if urlS == urlF and count == 2:
-        rezText = 'Yes'
-        break
-    count += 1
-
-print(rezText)
+    if urlF in reRez:
+        rez = 'Yes'
+print (rez)
